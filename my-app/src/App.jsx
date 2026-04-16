46fb49b46062350f012f7c372fc70928898d89ad
harrydeng104 <harrydeng104@gmail.com>
2026-04-15 22:43:09 -0400

    initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+// import { collection, getDocs, doc, setDoc, deleteDoc } from 'firebase/firestore'
+// import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+// import { db, auth } from './firebase'
+
+function App() {
+    return (
+      <header className="text-center p-2">
+          <h1 className="text-4xl font-bold">Pigeonhole Song Placements</h1>
+      </header>
+    )
+}
+
+export default App
